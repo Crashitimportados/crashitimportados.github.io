0573ed509fad300b460b14500993d679042440e3
EMISPARRA <emisparra@gmail.com>
2023-02-17 17:43:54 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,121 @@
+let shopItemsData = [
+    {
+      id: "vapo1",
+      name: "Supreme Epic Blue Raspberry",
+      price: 8500,
+      desc: "5000 Puffs",
+      img: "images/34.jpeg"
+    },
+  
+    { id: "vapo2",
+      name: "Supreme Epic Watermelon Candy",
+      price: 8500,
+      desc: "5000 Puffs",
+      img: "images/32.jpeg"
+    },
+  
+     { id: "vapo3",
+      name: "Supreme Epic+ Banana Ice",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/40.jpeg"
+    },
+  
+    { id: "vapo4",
+      name: "Supreme Epic+ Purple Rain",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/42.jpeg"
+    },
+  
+    { id: "vapo5",
+      name: "Supreme Epic+ Strawberry Watermelon",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/39.jpeg"
+    },
+  
+    { id: "vapo6",
+      name: "Supreme Epic+ Watermelon Candy",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/37.jpeg"
+    },
+  
+    { id: "vapo7",
+      name: "Supreme Epic+ Blueberry Mint",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/46.jpeg"
+    },
+
+    { id: "vapo8",
+      name: "Supreme Epic+ Black Ice",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/61.jpeg"
+    },
+  
+    { id: "vapo9",
+      name: "Supreme Epic+ Lush Ice",
+      price: 9500,
+      desc: "7000 Puffs Recargable",
+      img: "images/62.jpeg"
+    },
+
+    { id: "vapo10",
+    name: "Supreme Epic+ Grape Ice",
+    price: 9500,
+    desc: "7000 Puffs Recargable",
+    img: "images/63.jpeg"
+   },
+
+  { id: "vapo11",
+    name: "Supreme Prime Blue Ice",
+    price: 8000,
+    desc: "3500 Puffs",
+    img: "images/33.jpeg"
+  },
+
+  { id: "vapo12",
+    name: "Supreme Prime Strawberry Banana",
+    price: 8000,
+    desc: "3500 Puffs",
+    img: "images/45.jpeg"
+  },
+
+  { id: "vapo13",
+    name: "Supreme Prime Melon Berry",
+    price: 8000,
+    desc: "3500 Puffs",
+    img: "images/16.jpeg"
+  },
+
+  { id: "vapo14",
+    name: "Supreme Zero Strawberry Watermelon Candy",
+    price: 7000,
+    desc: "2000 Puffs",
+    img: "images/1.jpg"
+  },
+
+  { id: "vapo15",
+    name: "Supreme Zero Red Apple",
+    price: 7000,
+    desc: "2000 Puffs",
+    img: "images/2.jpg"
+  },
+
+  { id: "vapo16",
+    name: "Supreme Zero Black Ice",
+    price: 7000,
+    desc: "2000 Puffs",
+    img: "images/3.jpg"
+  },
+
+  { id: "vapo17",
+    name: "Supreme Fumar Energy Cranberry",
+    price: 9000,
+    desc: "5400 Puffs",
+    img: "images/4.jpg"
+  },
+ ];
