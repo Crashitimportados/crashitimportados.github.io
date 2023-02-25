c442c04f874daf1ee1adb119c2916ca2301568cc
EMISPARRA <emisparra@gmail.com>
2023-02-25 00:53:59 -0300

    agregados

--- src/data.js
+++ src/data.js
@@ -113,9 +113,72 @@ let shopItemsData = [
   },
 
   { id: "vapo17",
+    name: "Supreme Zero Peach Berry Ice",
+    price: 7000,
+    desc: "2000 Puffs",
+    img: "images/64.jpg"
+  },
+
+  { id: "vapo18",
+  name: "Supreme Zero Blueberry Mint",
+  price: 7000,
+  desc: "2000 Puffs",
+  img: "images/65.jpg"
+  },
+
+  { id: "vapo18",
     name: "Supreme Fumar Energy Cranberry",
     price: 9000,
     desc: "5400 Puffs",
     img: "images/4.jpg"
+  },
+
+  { id: "vapo19",
+  name: "Fume Unlimited Strawberry Watermelon",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/66.jpg"
+  },
+
+  { id: "vapo20",
+  name: "Fume Unlimited Black Ice",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/67.jpg"
+  },
+
+  { id: "vapo21",
+  name: "Fume Unlimited Blueberry Lemonade",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/68.jpg"
+  },
+
+  { id: "vapo22",
+  name: "Fume Unlimited Blueberry Mint",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/69.jpg"
+  },
+
+  { id: "vapo23",
+  name: "Fume Unlimited Purple Rain",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/70.jpg"
+  },
+
+  { id: "vapo24",
+  name: "Fume Unlimited Peach Mango Watermelon",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/71.jpg"
+  },
+
+  { id: "vapo25",
+  name: "Fume Unlimited Cotton Candy",
+  price: 9500,
+  desc: "7000 Puffs Recargable",
+  img: "images/72.jpg"
   },
  ];
