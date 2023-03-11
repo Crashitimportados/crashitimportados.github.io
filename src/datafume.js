329335bdda4ae8dd63e1dbb124f934a6a5b8556a
EMISPARRA <emisparra@gmail.com>
2023-03-11 00:32:02 -0300

    agregado

--- src/datafume.js
+++ src/datafume.js
@@ -4,49 +4,49 @@ let shopItemsData = [
   name: "Fume Unlimited Strawberry Watermelon",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/66.jpg"
+  img: "images/fume/66c.jpg"
   },
 
   { id: "vapo20",
   name: "Fume Unlimited Black Ice",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/67.jpg"
+  img: "images/fume/67c.jpg"
   },
 
   { id: "vapo21",
   name: "Fume Unlimited Blueberry Lemonade",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/68.jpg"
+  img: "images/fume/68c.jpg"
   },
 
   { id: "vapo22",
   name: "Fume Unlimited Blueberry Mint",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/69.jpg"
+  img: "images/fume/69b.jpg"
   },
 
   { id: "vapo23",
   name: "Fume Unlimited Purple Rain",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/70.jpg"
+  img: "images/fume/70b.jpg"
   },
 
   { id: "vapo24",
   name: "Fume Unlimited Peach Mango Watermelon",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/71.jpg"
+  img: "images/fume/71b.jpg"
   },
 
   { id: "vapo25",
   name: "Fume Unlimited Cotton Candy",
   price: 9500,
   desc: "7000 Puffs Recargable",
-  img: "images/fume/72.jpg"
+  img: "images/fume/72b.jpg"
   },
 
   
