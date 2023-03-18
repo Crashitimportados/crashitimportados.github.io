3d5c175080f5c233ec47f01e789cdf69875ccc7a
EMISPARRA <emisparra@gmail.com>
2023-03-18 00:06:43 -0300

    agregado

---
+++ src/datarandm.js
@@ -0,0 +1,45 @@
+let shopItemsData = [
+  
+    { id: "vapo16",
+    name: "Randm Tornado Strawberry Kiwi",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/1a.jpg"
+    },
+    
+    { id: "vapo16",
+    name: "Randm Tornado Strawberry Banana",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/2a.jpg"
+    },
+    
+    { id: "vapo16",
+    name: "Randm Tornado Banana Ice",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/3a.jpg"
+    },
+    
+    { id: "vapo16",
+    name: "Randm Tornado Cool Mint",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/4a.jpg"
+    },
+  
+    { id: "vapo16",
+    name: "Randm Tornado Strawberry Ice",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/5a.jpg"
+    },
+  
+    { id: "vapo16",
+    name: "Randm Tornado Kiwi Passion Fruit Guava",
+    price: 9000,
+    desc: "7000 Puffs",
+    img: "images/randm/6a.jpg"
+    },
+  
+    ];
